0bebfbf976956fd3cb8693c1038b1ad8daa9c008
michalis-oik <mi8chal@gmail.com>
2025-09-04 12:36:40 +0300

    changes so it becomes the package

--- Assets/_Scripts/_ImageTrackingPackage/URLTrackedImage.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class URLTrackedImage
-{
-    [SerializeField] public string name;
-    [SerializeField] public string url;
-    [SerializeField] public float physicalImageSize = 0.1f;
-}
--- Assets/_Scripts/_ImageTrackingPackage/URLTrackedImageCollection.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class URLTrackedImageCollection
-{
-    [SerializeField] public List<URLTrackedImage> uRLTrackedImages = new List<URLTrackedImage>();
-}
